9ccfc4656eb83b8f2765eb5dc3542889d5fb6b26
Jumbo Carlos <erraez.edinson@gmail.com>
2018-09-04 21:22:29 -0500

    Sylapp v2.0

--- static/js/efectos.js
+++ static/js/efectos.js
@@ -2,13 +2,19 @@
     var menuLateral = document.getElementById('open-hide');
     var btnMenu = document.getElementById("open-hide-btn");
     var btnMenuQuery = document.getElementById("open-hide-btn-query");
+    var closeMenuQuery = document.getElementById('close-menu-query');
     btnMenu.addEventListener('click', ()=>{
         menuLateral.classList.toggle('show-menu');
+    });
+
     btnMenuQuery.addEventListener('click', ()=>{
         menuLateral.classList.toggle('query-sidebar');
+        closeMenuQuery.classList.toggle('query-close-icon');
     });
-});
 
-
+    closeMenuQuery.addEventListener('click', ()=>{
+        menuLateral.classList.toggle('query-sidebar');
+        closeMenuQuery.classList.toggle('query-close-icon');
+    });
 })();
 
